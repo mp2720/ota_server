03a230f2acba15970a617ccb41283e0b84944e8f
Salomakhin Timur <sergeyrunov22@yandex.ru>
2024-01-22 16:58:50 +0700

    cli token generation added

--- cli.go
+++ cli.go
@@ -1,5 +1,49 @@
 package main
 
-func ExecuteCliCommands() {
-    // TODO: implement token generation token.
+import (
+	"fmt"
+	"os"
+)
+
+type CliInvalidUsageError struct{}
+
+func (e CliInvalidUsageError) Error() string {
+	return fmt.Sprintf("Invalid usage, type %s -h to get help", os.Args[0])
+}
+
+type CliService struct {
+	tokenSvc *TokenService
+	args     []string
+}
+
+func (svc *CliService) ExecuteCliCommands() (string, error) {
+	if len(svc.args) < 3 || len(svc.args) > 4 {
+		return "", CliInvalidUsageError{}
+	}
+
+	if svc.args[1] != "token" {
+		return "", CliInvalidUsageError{}
+	}
+
+	var (
+		isBoard bool
+		sub     string
+	)
+
+	if len(svc.args) == 4 {
+		if svc.args[3] != "-b" {
+			return "", CliInvalidUsageError{}
+		}
+
+		isBoard = true
+	} else {
+		isBoard = false
+	}
+
+	sub = svc.args[2]
+
+	return svc.tokenSvc.New(&TokenSubject{
+		sub,
+		isBoard,
+	})
 }
--- main.go
+++ main.go
@@ -17,6 +17,8 @@ func main() {
 		panic(err)
 	}
 
+	tokenSvc := TokenService{cfg}
+
 	if len(os.Args) == 1 {
 		db, err := NewDB(cfg)
 		if err != nil {
@@ -41,6 +43,15 @@ func main() {
 		fmt.Printf("\t-b - if subject is board\n")
 		os.Exit(0)
 	} else {
-		ExecuteCliCommands()
+        cliSvc := CliService{
+            &tokenSvc,
+            os.Args,
+        }
+        result, err := cliSvc.ExecuteCliCommands()
+        if err != nil {
+            panic(err)
+        }
+
+        fmt.Println(result)
 	}
 }
